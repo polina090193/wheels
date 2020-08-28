6fee9d7c4b1d0b2dd31d48b99690556edcd53014
polina090193 <polina_0901@mail.ru>
2020-08-28 11:24:46 +0300

    Add insta hovers, begin statistic animation

--- js/index.js
+++ js/index.js
@@ -1,17 +1,14 @@
-let menuButton = document.querySelector('.dropdown-menu-header');
-let dropdownMenu = document.querySelector('.dropdown-menu-list');
+let menuButton = document.querySelector('.dropdown-menu-header'),
+    dropdownMenu = document.querySelector('.dropdown-menu-list');
 
 menuButton.onclick = () => dropdownMenu.classList.toggle('dropdown-menu-list-closed');
 
-let productItems = document.querySelector('.product-items');
-let instaBlock = document.querySelector('.social-showcase');
-let instaItems = document.querySelectorAll('.instagram-photo'), photo;
+let productItems = document.querySelector('.product-items'),
+    instaBlock = document.querySelector('.social-showcase');
 
 productItems.addEventListener('click', showProductPopup);
 
-for(photo of instaItems) {
-    photo.addEventListener('click', showInstaPopup);
-}
+
 
 function showProductPopup(){
     let target = event.target;
@@ -46,12 +43,25 @@ function showProductPopup(){
     setTimeout(addWindowPopupClosing, 0, productPopUp);
 }
 
+
+let instaItems = document.querySelectorAll('.instagram-gallery a'), photo;
+
+for(photo of instaItems) {
+    photo.addEventListener('touchend', showInstaPopup);
+}
+
 function showInstaPopup(){
 
+    console.log(event.target)
+
     let instaPopUp = document.createElement('figure');
-    instaPopUp.className = "insta-popup";
+        instaPopUp.className = "insta-popup";
+
+    let instaPreview = event.target.parentNode.querySelector('.instagram-preview');
 
     if(document.querySelector('.insta-popup') !== null) return;
+
+    instaPreview.style.display = 'none';
     instaPopUp.innerHTML = event.target.parentNode.innerHTML;
 
     instaBlock.append(instaPopUp);
@@ -73,4 +83,41 @@ function addClosePopupButton(elem) {
 
 function addWindowPopupClosing(elem) {
     document.body.addEventListener('click', () => elem.remove());
+}
+
+
+let statistic = document.querySelector('.statistics');
+    currentBikesSold = 409;
+    
+function isStatisticVisible() {
+    let statisticVisible = statistic.getBoundingClientRect(),
+    windowHeight = document.documentElement.clientHeight;
+
+    let topVisible = statisticVisible.top > windowHeight && statisticVisible.top < 0;
+
+    return topVisible
+}
+
+function statisticCounter() {
+        
+    if (isStatisticVisible()) {
+        growingStatistic = document.querySelector('.statistics-item-value__bikes'),
+        growingStatisticValue = 0;
+        growStatistic()
+    }
+}
+
+function growStatistic() {
+    growingStatisticValue += 1;
+    growingStatistic.innerHTML = growingStatisticValue;
+    if (growingStatisticValue < currentBikesSold){
+        setTimeout(growStatistic, 5)
+    }
+}
+
+function onScroll() {
+    if (!isStatisticVisible()) {
+        window.onscroll = statisticCounter;
+        return true
+    }
 }
